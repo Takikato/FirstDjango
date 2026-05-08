9653853d380a68643962597a15c27e07a44bfaf7
Takikato <lian1446ellis@gmail.com>
2026-05-08 17:27:53 +0200

    First commit

---
+++ sticky_notes/notes/admin.py
@@ -0,0 +1,9 @@
+# notes/admin.py
+from django.contrib import admin
+from .models import Note
+from .models import Author
+# Register your models here.
+# Note model
+admin.site.register(Note)
+# Author model
+admin.site.register(Author)
